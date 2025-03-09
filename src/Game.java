2f9d391656754507551493cd29d329dacf7652d5
Frede-72 <frjo0005@stud.kea.dk>
2025-03-09 12:27:07 +0100

    Added method for only allowing an integer as input

--- src/Game.java
+++ src/Game.java
@@ -9,24 +9,24 @@ public class Game {
 
         Scanner scanner = new Scanner(System.in);
         Tamagotchi tamagotchi = newTamagotchi(scanner);
-        tamagotchi.makeSound();
-        while(true){
-            if(!tamagotchi.checkLiveStatus()){
+        while (true) {
+            if (!tamagotchi.checkLiveStatus()) {
                 break;
             }
+            tamagotchi.makeSound();
             System.out.println("What would you like to do");
             System.out.println("1 - play");
             System.out.println("2 - sleep");
             System.out.println("3 - feed");
-            int choice = scanner.nextInt();
+            int choice = scanInt(scanner, "Input must be a number");
             scanner.nextLine();
-            if(choice==1){
+            if (choice == 1) {
                 tamagotchi.play();
             }
-            if(choice==2){
+            if (choice == 2) {
                 tamagotchi.sleep();
             }
-            if(choice==3){
+            if (choice == 3) {
                 tamagotchi.feed();
             }
         }
@@ -36,7 +36,7 @@ public class Game {
     public static Tamagotchi newTamagotchi(Scanner sc) {
         while (true) {
             System.out.println("Choose animal: Type 1 for cat and 2 for dog");
-            int choice = sc.nextInt();
+            int choice = scanInt(sc, "Input must be a number");
             sc.nextLine();
             if (choice == 1) {
                 System.out.println("Name your pet");
@@ -52,5 +52,14 @@ public class Game {
             }
         }
     }
+
+    public static int scanInt(Scanner scanner, String errormessage) {
+        while (!(scanner.hasNextInt())) {
+            System.out.println(errormessage);
+            scanner.nextLine();
+        }
+        return scanner.nextInt();
+    }
+
 }
 
